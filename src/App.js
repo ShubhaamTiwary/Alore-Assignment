b270e88c07fc1cbff31ffe0abe9c0eea2b9bc0c6
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-09-16 17:50:16 +0530

    scroll

--- src/App.js
+++ src/App.js
@@ -3,66 +3,113 @@ import Segment from './component/Segment'
 import './App.css'
 
 function App() {
-  const [fileObj,setFileObj]=useState([
+  const [fileObj, setFileObj] = useState([
     {
-      name:'Segment 1',
-      icon:'🐔',
+      name: 'Segment 1',
+      icon: '🐔',
       id: 1,
-      files:[
-       {
-         name: 'Newyork-ITeS-11-50',
-         icon: '😀',
-         color: 'red'
-       },
-       {
-         name: 'Newyork-ITeS-11-50',
-         icon: '🤩',
-         color: 'green'
-       }
+      files: [
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '😀',
+          color: 'red'
+        },
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '🤩',
+          color: 'green'
+        }
       ]
-    } ,
+    },
     {
-      name:'Segment 2',
-      icon:'😎',
+      name: 'Segment 2',
+      icon: '😎',
       id: 1,
-      files:[
-       {
-         name: 'Newyork-ITeS-11-50',
-         icon: '😛',
-         color: 'purple'
-       },
-       {
-         name: 'Newyork-ITeS-11-50',
-         icon: '😇',
-         color: 'blue'
-       },
-       {
-        name: 'Newyork-ITeS-11-50',
-        icon: '😬',
-        color: 'orange'
-      }
+      files: [
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '😛',
+          color: 'purple'
+        },
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '😇',
+          color: 'blue'
+        },
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '😬',
+          color: 'orange'
+        }
+      ]
+    },
+    {
+      name: 'Segment 2',
+      icon: '😎',
+      id: 1,
+      files: [
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '😛',
+          color: 'purple'
+        },
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '😇',
+          color: 'blue'
+        },
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '😬',
+          color: 'orange'
+        }
+      ]
+    }
+    ,
+    {
+      name: 'Segment 2',
+      icon: '😎',
+      id: 1,
+      files: [
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '😛',
+          color: 'purple'
+        },
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '😇',
+          color: 'blue'
+        },
+        {
+          name: 'Newyork-ITeS-11-50',
+          icon: '😬',
+          color: 'orange'
+        }
       ]
     }
   ]);
-  const [id,setId]=useState(0);
+  const [id, setId] = useState(0);
 
   return (
     <div className='MainDiv'>
       <div className='sideBar'>
         <span>Prospector</span>
-        <span id='SegmentsHeader'>Segments</span>
-        {fileObj.map((ele)=>{
-          return< span id='segmentText'>{ele.icon} {ele.name}</span>
+        <span id='SegmentsHeader'>SEGMENTS</span>
+        {fileObj.map((ele) => {
+          return <span id='segmentText'>{ele.icon} {ele.name}</span>
         })}
       </div>
 
-    <div id="App">
-      {fileObj.map((ele)=>{
-        return <Segment ele={ele}/>
-      })}
+      <div id="App">
+        <div id='header'>
+        </div>
+        {fileObj.map((ele) => {
+          return <Segment ele={ele} />
+        })}
+      </div>
     </div>
-    </div>
-    
+
   );
 }
 
