a54a1f0c2d350ac61bbd3c049cf15137d6569ac2
Shubhaam Tiwary <shubhaamtiwary@gmail.com>
2023-09-18 01:44:05 +0530

    Added Add Segment and Add table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,57 @@
 import React, { useState } from 'react';
 import Segment from './component/Segment'
 import './App.css'
+import InputEmoji from 'react-input-emoji'
+
+const AddSegmentComponent=({setShowSegment,fileObj,setFileObj,id,setId})=>{
+  // console.log("ele",ele);
+  const onAdd=()=>{
+    let newFileObj=fileObj;
+    newFileObj.push({
+      name:name,
+      icon:icon,
+      id:id,
+      files:[]
+    })
+    setId(id+1);
+    setFileObj(newFileObj);
+    setShowSegment(false);
+  }
+  const [name,setName]=useState('');
+  const [icon,setIcon]=useState('');
+  const [color,setColor]=useState('');
+  return(
+    <div className="modal-overlay">
+      <div className="table">
+        <div className="table-header ">
+          <h2>Add Segment</h2>
+          <button className="close-button" onClick={()=>{setShowSegment(false)}} >X</button>
+        </div>
+        <div className="table-body">
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <InputEmoji
+            type="text"
+            placeholder="Icon"
+            value={icon}
+            onChange={(e) => setIcon(e)}
+          />
+        </div>
+        <div className="table-footer">
+          <button className="add-button" onClick={onAdd}>Add</button>
+          <button className="cancel-button" onClick={()=>{setShowSegment(false)}}>Cancel</button>
+        </div>
+      </div>
+    </div>
+  )
+}
 
 function App() {
+  const [showSegment,setShowSegment]=useState(false);
   const [fileObj, setFileObj] = useState([
     {
       name: 'Segment 1',
@@ -20,47 +69,27 @@ function App() {
           color: 'green'
         }
       ]
-    },
-    {
-      name: 'Segment 2',
-      icon: '😎',
-      id: 1,
-      files: [
-        {
-          name: 'Newyork-ITeS-11-50',
-          icon: '😛',
-          color: 'purple'
-        },
-        {
-          name: 'Newyork-ITeS-11-50',
-          icon: '😇',
-          color: 'blue'
-        },
-        {
-          name: 'Newyork-ITeS-11-50',
-          icon: '😬',
-          color: 'orange'
-        }
-      ]
     }
   ]);
-  const [id, setId] = useState(0);
+  const [id, setId] = useState(1);
 
   return (
     <div className='MainDiv'>
+      {showSegment && <AddSegmentComponent setShowSegment={setShowSegment} fileObj={fileObj} setFileObj={setFileObj} id={id} setId={setId}/>}
       <div className='sideBar'>
         <span>Prospector</span>
         <span id='SegmentsHeader'>SEGMENTS</span>
         {fileObj.map((ele) => {
           return <span id='segmentText'>{ele.icon} {ele.name}</span>
         })}
+        <button style={{width:100, marginTop:10, marginLeft:20}} onClick={()=>{setShowSegment(true)}}>Add Segment</button>
       </div>
 
       <div id="App">
         <div id='header'>
         </div>
         {fileObj.map((ele) => {
-          return <Segment ele={ele} />
+          return <Segment ele={ele} fileObj={fileObj} setFileObj={setFileObj}/>
         })}
       </div>
 
--- src/component/Segment.js
+++ src/component/Segment.js
@@ -2,11 +2,75 @@ import React, { useState } from 'react'
 import TableCard from './TableCard'
 import Black from '../utils/black-dropdown.svg';
 import Blue from '../utils/blue-dropdown.svg';
+import InputEmoji from 'react-input-emoji'
 
-const Segment = ({ ele }) => {
+const AddTableComponent=({setAddTable,ele,fileObj,setFileObj})=>{
+  // console.log("ele",ele);
+  const onAdd=()=>{
+    let newFileObj=fileObj;
+    newFileObj.forEach((item)=>{
+      if(item.name==ele.name){
+        item.files.push({
+          name:name,
+          icon:icon,
+          color:color
+        })
+      }
+    })
+    console.log(newFileObj);
+    setFileObj(newFileObj);
+    setAddTable(false);
+  }
+  const [name,setName]=useState('');
+  const [icon,setIcon]=useState('');
+  const [color,setColor]=useState('');
+  return(
+    <div className="modal-overlay">
+      <div className="table">
+        <div className="table-header ">
+          <h2>Add Table</h2>
+          <button className="close-button" onClick={()=>{setAddTable(false)}} >X</button>
+        </div>
+        <div className="table-body">
+          <span>Name</span>
+          <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <span>Icon</span>
+          <InputEmoji
+            type="text"
+            placeholder="Icon"
+            value={icon}
+            onChange={(e) => setIcon(e)}
+          />
+
+          <span>Color</span>
+          <input
+            type="color"
+            placeholder="Color"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+          />
+        </div>
+        <div className="table-footer">
+          <button className="add-button" onClick={onAdd}>Add</button>
+          <button className="cancel-button" onClick={()=>{setAddTable(false)}}>Cancel</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+const Segment = ({ ele,fileObj,setFileObj }) => {
   const [show, setShow] = useState(false);
+  const [addTable,setAddTable] = useState(false);
   return (
     <div id='segment'>
+      {addTable && <AddTableComponent setAddTable={setAddTable} ele={ele} fileObj={fileObj} setFileObj={setFileObj} />}
       <span id='segHeader' >{ele.icon} {ele.name}</span>
       <span><img src={show ? Blue : Black} id='icon' onClick={() => { setShow(!show) }} /></span>
       {show &&
@@ -16,12 +80,11 @@ const Segment = ({ ele }) => {
           })}
           <div className='TableCard'>
             <div id='label'>Add a new Table</div>
-            <div id='outerCircle2'>
-              +
+            <div id='outerCircle2' >
+              <span onClick={()=>{setAddTable(true)}}>+</span>
             </div>
           </div>
         </div>}
-
     </div>
   )
 }
